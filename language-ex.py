101e0870ee7524805984f583cb5f1343cb9ab7f2
Mihail Marian <mihailmarian12@gmail.com>
2021-01-21 10:23:58 +0000

    finished ex 2

--- language-ex.py
+++ language-ex.py
@@ -3,3 +3,7 @@
 NAME = "Jon Jones"
 
 print("Hello " + NAME + ", would you like to learn some Python today?")
+
+print("Hello " + NAME.lower() + ", would you like to learn some Python today?")
+
+print("Hello " + NAME.upper() + ", would you like to learn some Python today?")
