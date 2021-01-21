7b2393bad024c8650b580ce561e05e0a876a6ca8
Mihail Marian <mihailmarian12@gmail.com>
2021-01-21 10:30:05 +0000

    finished string

---
+++ quote.py
@@ -0,0 +1,8 @@
+"""string chapter"""
+
+NAME = "Albert Einstein"
+QUOTE = "A person who never made a mistake never tried anything new."
+
+MESSAGE = NAME + ' once said, "' + QUOTE + '"'
+
+print(MESSAGE)
